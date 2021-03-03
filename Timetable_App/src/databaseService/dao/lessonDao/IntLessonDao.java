69245b8f91a9c7eff59c0101b9751809651b4dfa
Fatima-Ezzahra Lahnine <mfati1315@gmail.com>
2021-03-03 22:02:28 +0100

    correction select details

--- Timetable_App/src/databaseService/dao/lessonDao/IntLessonDao.java
+++ Timetable_App/src/databaseService/dao/lessonDao/IntLessonDao.java
@@ -1,8 +1,7 @@
 package databaseService.dao.lessonDao;
 
-import java.util.ArrayList;
+
 import java.util.List;
-import java.util.ArrayList;
 
 import databaseService.beans.Lesson;
 
@@ -12,5 +11,5 @@ public interface IntLessonDao {
 	public boolean updateLesson(Lesson ls);
 	public List<Lesson> selectLesson(Lesson ls);
 	public List<Lesson> selectUniqueLessonTimetable(Lesson ls);
-	public ArrayList<String> selectDetailsLesson(Lesson ls);
+	public List<Lesson> selectDetailsLesson(Lesson ls);
 }
